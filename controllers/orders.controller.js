2864199325e7b28814a486a6285c0d32f6c671b4
abelraju <abelraju12@gmail.com>
2023-08-09 18:06:03 +0530

    password validation

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -64,33 +64,38 @@ async function addOrder(req, res, next) {
     next(error);
     return;
   }
-  let tempCart = [];
-  cart.items.map(async (item) => {
-    tempCart.push(item.product.id);
-  });
-  req.session.cart = null;
-  const session = await stripe.checkout.sessions.create({
-    line_items: cart.items.map(function (item) {
-      return {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price_data: {
-          currency: "inr",
-          product_data: {
-            name: item.product.title,
+  try{
+    let tempCart = [];
+    cart.items.map(async (item) => {
+      tempCart.push(item.product.id);
+    });
+    req.session.cart = null;
+    const session = await stripe.checkout.sessions.create({
+      line_items: cart.items.map(function (item) {
+        return {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: item.product.title,
+            },
+            unit_amount: +item.product.price.toFixed(2) * 100,
           },
-          unit_amount: +item.product.price.toFixed(2) * 100,
-        },
-        quantity: item.quantity,
-      };
-    }),
-    mode: "payment",
-    success_url: `http://localhost:3000/orders/success?cart=${JSON.stringify(
-      tempCart
-    )}&paymentmode=stripe&orderId=${orderDetails.insertedId.toString()}`,
-    cancel_url: `http://localhost:3000/orders/failure`,
-  });
+          quantity: item.quantity,
+        };
+      }),
+      mode: "payment",
+      success_url: `http://localhost:3000/orders/success?cart=${JSON.stringify(
+        tempCart
+      )}&paymentmode=stripe&orderId=${orderDetails.insertedId.toString()}`,
+      cancel_url: `http://localhost:3000/orders/failure`,
+    });
 
-  res.redirect(303, session.url);
+    res.redirect(303, session.url);
+  } catch (error){
+    next(error);
+    return;
+  }
 }
 async function paymentUpdate(req, res) {
   let { orderId } = req.body;
